8a507aea3177251880b070fa9b7025de64412724
Adam Král <adam.kral@kiwi.com>
2018-09-30 18:03:10 +0200

    from data methods, annotations

--- 02/scorelib.py
+++ 02/scorelib.py
@@ -1,40 +1,90 @@
-class Print:
-    def __init__(self, edition, print_id, partiture):
-        self.edition = edition
-        self.print_id = print_id
-        self.partiture = partiture
-
-    def format(self):
-        if self.print_id:
-            print("Print Number: " + self.print_id)
-        print(self.edition.format())
-        if self.partiture:
-            print("Partiture: " + self.partiture)
-
-    def composition(self):
-        return self.edition.composition
+import re
 
 
-class Edition:
-    def __init__(self, composition, authors, name):
-        self.composition = composition
-        self.authors = authors
+class Voice:
+    def __init__(self, name: str=None, range: str=None):
         self.name = name
+        self.range = range
 
     def format(self):
-        string = self.composition.format()
-        if self.authors:
+        string = ""
+        if self.name or self.range:
+            string += "Voice: "
+            if self.name:
+                string += self.name
+            if self.range:
+                string += "; " + self.range
             string += "\n"
-        for author in self.authors:
-            string += "Editor: " + author.format()
-        if self.name:
-            string += "Edition: " + self.name + "\n"
+        return string
 
+    @staticmethod
+    def fromData(data):
+        voiceRange = re.match(r"(.*)--([^,;]*)[,;](.*)", data)
+        if voiceRange:
+            return Voice(voiceRange.group(3).strip(), voiceRange.group(1) + "--" + voiceRange.group(2))
+        else:
+            return Voice(data)
+
+
+class Person:
+    def __init__(self, name: str, born: int = None, died: int = None):
+        self.name = name
+        self.born = born
+        self.died = died
+
+    def format(self):
+        string = ""
+        if self.name:
+            string += self.name
+        if self.born or self.died:
+            lifeString = "--"
+            if self.born:
+                lifeString = str(self.born) + lifeString
+            if self.died:
+                lifeString += str(self.died)
+            string += lifeString
         return string
 
 
+    @staticmethod
+    def fromData(data):
+        if data:
+            insideBrackets = re.search(r'(.*)\((.*?)\)', data)
+            if insideBrackets:
+                name = insideBrackets.group(1)
+                years = insideBrackets.group(2)
+                yearsSplit = re.search(r'(\d\d\d\d)(-|--)(\d\d\d\d)', years)
+                if yearsSplit:
+                    return Person(name, int(yearsSplit.group(1)), int(yearsSplit.group(3)))
+                else:
+                    yearBorn = re.search(r'(\d\d\d\d)(-|--)(.*)', years)
+                    if yearBorn:
+                        return Person(name, int(yearBorn.group(1)))
+                    else:  # find * and +
+                        yearBorn = None
+                        yearDied = None
+                        yearDiedReg = re.search(r'(\+)(\d\d\d\d)', years)
+                        if yearDiedReg:
+                            yearDied = int(yearDiedReg.group(2))
+                        yearBornReg = re.search(r'(\*)(\d\d\d\d)', years)
+                        if yearBornReg:
+                            yearBorn = int(yearBornReg.group(2))
+                        return Person(name, yearBorn, yearDied)
+            else:
+                return Person(data)
+
+
 class Composition:
-    def __init__(self, name, incipit, key, genre, year, voices, authors):
+    def __init__(
+            self,
+            name: str=None,
+            incipit: str=None,
+            key: str=None,
+            genre: str=None,
+            year: int=None,
+            voices: [Voice] = (),
+            authors: [Person] = ()
+    ):
         self.name = name
         self.incipit = incipit
         self.key = key
@@ -62,39 +112,103 @@ class Composition:
         return string
 
 
-class Voice:
-    def __init__(self, name, range):
+    @staticmethod
+    def fromData(data):
+        voices = []
+        for key, val in data.items():
+            if re.match(r"(.*)Voice(.*)", key):
+                voices.append(Voice.fromData(val))
+
+        composers = []
+        if "Composer" in data:
+            for composer in data["Composer"].split(";"):
+                if Person.fromData(composer):
+                    composers.append(Person.fromData(composer))
+
+        return Composition(
+            data["Title"] if "Title" in data else None,
+            data["Incipit"] if "Incipit" in data else None,
+            data["Key"] if "Key" in data else None,
+            data["Genre"] if "Genre" in data else None,
+            Composition.processCompositionYear(data["Composition Year"]) if "Composition Year" in data else None,
+            voices,
+            composers
+        )
+
+
+    @staticmethod
+    def processCompositionYear(data):
+        regexp = re.compile('(.*)(\d\d\d\d)(.*)')
+        match = regexp.match(data)
+        return match.group(1) if match else None
+
+
+class Edition:
+    def __init__(
+            self,
+            composition: Composition,
+            authors: [Person] = (),
+            name: str = None
+    ):
+        self.composition = composition
+        self.authors = authors
         self.name = name
-        self.range = range
 
     def format(self):
-        string = ""
-        if self.name:
-            string += "Voice: " + self.name
-        if self.range:
-            string += ", Range: " + self.range
-
-        if self.name or self.range:
+        string = self.composition.format()
+        if self.authors:
             string += "\n"
+        for author in self.authors:
+            string += "Editor: " + author.format() + "\n"
+        if self.name:
+            string += "Edition: " + self.name + "\n"
+
         return string
 
 
-class Person:
-    def __init__(self, name, born, died):
-        self.name = name
-        self.born = born
-        self.died = died
+    @staticmethod
+    def fromData(data, composition):
+        editors = []
+        if "Editor" in data:
+            splitEditors = data["Editor"].split(",")
+            joinedEditors = [''.join(x) for x in zip(splitEditors[0::2], splitEditors[1::2])]
+            for editor in joinedEditors:
+                if Person.fromData(editor):
+                    editors.append(Person.fromData(editor))
+        return Edition(
+            composition,
+            editors,
+            data["Edition"] if "Ediiton" in data else None
+        )
+
+
+class Print:
+    def __init__(self, edition: Edition, print_id: int, partiture: bool):
+        self.edition = edition
+        self.print_id = print_id
+        self.partiture = partiture
 
     def format(self):
-        string = ""
-        if self.name:
-            string += self.name
-        if self.born or self.died:
-            lifeString = "--"
-            if self.born:
-                lifeString = self.born + lifeString
-            if self.died:
-                lifeString += self.died
-            string += lifeString
+        if self.print_id:
+            print("Print Number: " + str(self.print_id))
+        print(self.edition.format())
+        print("Partiture: " + str(self.partiture))
 
-        return string
+    def composition(self):
+        return self.edition.composition
+
+    @staticmethod
+    def fromData(data):
+        if "Partiture" in data:
+            if re.search("yes", data["Partiture"].lower()):
+                partiture = True
+            else:
+                partiture = False
+        else:
+            partiture = False
+
+        return Print(
+            Edition.fromData(data, Composition.fromData(data)),
+            data["Print Number"] if "Print Number" in data else None,
+            partiture
+        )
--- 02/test.py
+++ 02/test.py
@@ -1,6 +1,5 @@
 import sys
-import re
-from scorelib import Print, Edition, Composition, Voice, Person
+from scorelib import Print
 
 
 def load(filename):
@@ -17,82 +16,8 @@ def load(filename):
     return prints
 
 
-def processCompositionYear(data):
-    regexp = re.compile('(.*)(\d\d\d\d)(.*)')
-    match = regexp.match(data)
-    return match.group(1) if match else None
-
-
-def processPerson(data):
-    if data:
-        insideBrackets = re.search(r'(.*)\((.*?)\)', data)
-        if insideBrackets:
-            name = insideBrackets.group(1)
-            years = insideBrackets.group(2)
-            yearsSplit = re.search(r'(\d\d\d\d)(-|--)(\d\d\d\d)', years)
-            if yearsSplit:
-                return Person(name, yearsSplit.group(1), yearsSplit.group(3))
-            else:
-                yearBorn = re.search(r'(\d\d\d\d)(-|--)(.*)', years)
-                if yearBorn:
-                    return Person(name, yearBorn.group(1), None)
-                else: # find * and +
-                    yearBorn = None
-                    yearDied = None
-                    yearDiedReg = re.search(r'(\+)(\d\d\d\d)', years)
-                    if yearDiedReg:
-                        yearDied = yearDiedReg.group(2)
-                    yearBornReg = re.search(r'(\*)(\d\d\d\d)', years)
-                    if yearBornReg:
-                        yearBorn = yearBornReg.group(2)
-                    return Person(name, yearBorn, yearDied)
-        else:
-            return Person(data, None, None)
-
-
 def processBlock(data):
-    voices = []
-    editors = []
-    composers = []
-    for key, val in data.items():
-        if re.match(r"(.*)Voice(.*)", key):
-            voices.append(processVoice(val))
-    if "Editor" in data:
-        if processPerson(data["Editor"]):
-            editors.append(processPerson(data["Editor"]))
-    if "Composer" in data:
-        for composer in data["Composer"].split(";"):
-            if processPerson(composer):
-                composers.append(processPerson(composer))
-    composition = Composition(
-        data["Title"] if "Title" in data else None,
-        data["Incipit"] if "Incipit" in data else None,
-        data["Key"] if "Key" in data else None,
-        data["Genre"] if "Genre" in data else None,
-        processCompositionYear(data["Composition Year"]) if "Composition Year" in data else None,
-        voices,
-        composers
-    )
-
-    edition = Edition(
-        composition,
-        editors,
-        data["Edition"] if "Ediiton" in data else None
-    )
-
-    return Print(
-        edition,
-        data["Print Number"] if "Print Number" in data else None,
-        data["Partiture"] if "Partiture" in data else None
-    )
-
-
-def processVoice(voiceData):
-    voiceRange = re.match(r"(.*)--([^,;]*)[,;](.*)", voiceData)
-    if voiceRange:
-        return Voice(voiceRange.group(3).strip(), voiceRange.group(1) + "--" + voiceRange.group(2))
-    else:
-        return Voice(voiceData, None)
+    return Print.fromData(data)
 
 
 printclasses = load(sys.argv[1])
