8a9f794a57a410cb8409c6bdf7e7fcc20324abd7
Adam Král <adam.kral@kiwi.com>
2018-12-04 20:47:25 +0100

    comment prints

--- 11/ttt.py
+++ 11/ttt.py
@@ -10,7 +10,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     def generateNewId(self):
         if self.gameBoards.__len__() == 0:
             return 1
-        print('keys {}'.format(self.gameBoards.keys()))
+        # print('keys {}'.format(self.gameBoards.keys()))
         return max(self.gameBoards) + 1
 
     def newBoard(self, boardId, name):
@@ -107,17 +107,17 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         query = urlparse(self.path).query
         path = urlparse(self.path).path
-        print("query {}".format(query))
+        # print("query {}".format(query))
         if not query:
             self.sendHeaders()
             self.doResponse(self.formatBadResponse("parameters not provided"))
             return
         query_components = dict(qc.split("=") for qc in query.split("&"))
-        print("query components {}".format(query_components))
-        self.sendHeaders()
+        # print("query components {}".format(query_components))
         if path == '/start':
+            self.sendHeaders()
             gameId = self.generateNewId()
-            print("id {}".format(gameId))
+            # print("id {}".format(gameId))
             if 'name' not in query_components:
                 outjson = self.formatBadResponse('parameter name not provided')
             else:
@@ -125,6 +125,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                 outjson = {"id": gameId}
 
             self.doResponse(outjson)
+            return
         elif path == '/status':
             self.sendHeaders()
             if 'game' not in query_components:
@@ -143,6 +144,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                         responseJson = copiedGameBoard
 
             self.doResponse(responseJson)
+            return
         elif path == '/play':
             self.sendHeaders()
             if 'game' not in query_components:
@@ -175,11 +177,12 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
                         self.switchToNextPlayer(playerNumber, boardGameId)
                         responseJson = {"status": "ok"}
                 except Exception as e:
-                    print("Exception {}".format(e))
+                    # print("Exception {}".format(e))
                     responseJson = self.formatBadResponse(e.args[0])
 
             self.doResponse(responseJson)
-        print("game boards {}".format(self.gameBoards))
+            return
+        # print("game boards {}".format(self.gameBoards))
 
 port = sys.argv[1]
 
